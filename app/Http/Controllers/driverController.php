3604e79a491e448fb1b5519bdeecf12c8d5edb62
Hefnawy1000 <mohamedhefnyhamam@gmail.com>
2022-03-12 12:46:40 +0200

    adding files

---
+++ app/Http/Controllers/driverController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class driverController extends Controller
+{
+     public function driversignup()
+    {
+        return view('dirversignup');
+    }
+
+    public function driverrigester(Request $request)
+    {
+        # code...
+    }
+}
